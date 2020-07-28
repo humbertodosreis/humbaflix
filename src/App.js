6582f25425835054cce3d35520c07d8868607e3a
Humberto dos Reis Rodrigues <rrodrigues.humberto@gmail.com>
2020-07-28 02:34:03 -0300

    Fist commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import dados from './data/dados_iniciais.json'
+import './App.css'
+import Menu from './components/Menu'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: '#141414' }}>
+      <Menu />
+      <BannerMain
+        videoTitle={dados.categorias[0].videos[0].titulo}
+        url={dados.categorias[0].videos[0].url}
+        videosDescription='O que faz uma desenvolvedora front-end? #HipstersPontoTube'
+      />
+      <Carousel ignoreFirstVideo category={dados.categorias[0]} />
+      <Carousel category={dados.categorias[1]} />
+      <Carousel category={dados.categorias[2]} />
+      <Carousel category={dados.categorias[3]} />
+      <Carousel category={dados.categorias[4]} />
+      <Carousel category={dados.categorias[5]} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
